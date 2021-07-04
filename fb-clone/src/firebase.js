20a22bc1b2cdd0009d2ad80be4e7d3a3bc6e94e7
DasMatej <dasmatej7@gmail.com>
2021-07-04 18:27:03 +0200

    Header Placement

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDHZyzWzoCV3mCf50T9n0y7HRkKmmo4rwU",
+    authDomain: "facebook-clone-91059.firebaseapp.com",
+    projectId: "facebook-clone-91059",
+    storageBucket: "facebook-clone-91059.appspot.com",
+    messagingSenderId: "990752412107",
+    appId: "1:990752412107:web:cfe159a342cd2ccd3aa64e",
+    measurementId: "G-33CQFJL3CC"
+  };
